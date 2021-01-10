6421f3f102316f2767ea3a05ce2c85acee9ba101
e.shakeri <QWEasd123>
2021-01-10 14:47:25 +0330

    Impl Sync produce event

--- events-producer/src/main/java/com/oms/controller/OrderEventController.java
+++ events-producer/src/main/java/com/oms/controller/OrderEventController.java
@@ -3,6 +3,7 @@ package com.oms.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.oms.domain.OrderEvent;
 import com.oms.producer.OrderEventProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/order-event")
+@Slf4j
 public class OrderEventController {
 
     private final OrderEventProducer orderEventProducer;
@@ -29,7 +31,9 @@ public class OrderEventController {
     @PostMapping
     public ResponseEntity<OrderEvent> postOrderEvent(@RequestBody OrderEvent orderEvent) throws JsonProcessingException {
 
+        log.info("before sentOrderEvent");
         orderEventProducer.sentOrderEvent(orderEvent);
+        log.info("after sentOrderEvent");
         return  ResponseEntity.status(HttpStatus.CREATED).body(orderEvent);
     }
 
--- events-producer/src/main/java/com/oms/producer/OrderEventProducer.java
+++ events-producer/src/main/java/com/oms/producer/OrderEventProducer.java
@@ -3,13 +3,14 @@ package com.oms.producer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oms.domain.OrderEvent;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author Ehsan Sh
@@ -19,15 +20,16 @@ import org.springframework.util.concurrent.ListenableFutureCallback;
 @Slf4j
 public class OrderEventProducer {
 
-    KafkaTemplate<Long,String> kafkaTemplate;
+    private final KafkaTemplate<Long,String> kafkaTemplate;
 
-    ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
     public OrderEventProducer(KafkaTemplate<Long, String> kafkaTemplate, ObjectMapper objectMapper) {
         this.kafkaTemplate = kafkaTemplate;
         this.objectMapper = objectMapper;
     }
 
+    //Async produce
     public void sentOrderEvent(OrderEvent orderEvent) throws JsonProcessingException {
 
         Long key = orderEvent.getId();
@@ -35,6 +37,27 @@ public class OrderEventProducer {
 
         ListenableFuture<SendResult<Long,String>> listenableFuture =  kafkaTemplate.sendDefault(key,value);
         listenableFuture.addCallback(new OrderEventListenableFutureCallback(key,value));
+
+    }
+
+    //Sync produce
+    public SendResult<Long, String> sendLibraryEventSynchronous(OrderEvent orderEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+
+        Long key = orderEvent.getId();
+        String value = objectMapper.writeValueAsString(orderEvent);
+
+        SendResult<Long,String> sendResult;
+        try {
+            sendResult = kafkaTemplate.sendDefault(key,value).get(1, TimeUnit.SECONDS);
+        } catch (ExecutionException | InterruptedException e) {
+            log.error("ExecutionException/InterruptedException Sending the Message and the exception is {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            log.error("Exception Sending the Message and the exception is {}", e.getMessage());
+            throw e;
+        }
+
+        return sendResult;
 
     }
 
